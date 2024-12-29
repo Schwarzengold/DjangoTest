40f3ae74838e4adc429d4645b85683e60c86d181
Schwarzengold <lia.rv.ua@gmail.com>
2024-12-29 13:52:05 +0200

    Added Azure domain to ALLOWED_HOSTS

--- firstdjango/settings.py
+++ firstdjango/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-%v4z=1s)r(ua_%c^*^r-3f3gj*3sgu!4e-n+)5gvm#%@r4lp01
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['firstdjangotest.azurewebsites.net']
 
 # Application definition
 
@@ -117,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
